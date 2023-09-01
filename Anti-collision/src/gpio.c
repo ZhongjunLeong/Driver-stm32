1455d4844af18121e804c34948cd2f6debf4db35
ZhongjunLeong <1074024793@qq.com>
2023-09-01 22:15:40 +0800

    update gpio

--- Anti-collision/src/gpio.c
+++ Anti-collision/src/gpio.c
@@ -1,10 +1,3 @@
-/**************************************
-*文件名：   dtsplatform.c
-*作者:      lzj
-*版本：     v1.0
-*描述：     使用设备树的platform框架，驱动led/PI0
-**************************************/
-
 #include <linux/types.h>
 #include <linux/kernel.h>
 #include <linux/delay.h>
@@ -27,70 +20,66 @@
 #include <asm/mach/map.h>
 #include <asm/uaccess.h>
 #include <asm/io.h>
+//#define PLATDRV_MAGIC       0x60     //魔术字
+//#define SET             _IO (PLATDRV_MAGIC, 0x18)   //没有数据传输的命令
+//#define OUT              _IO (PLATDRV_MAGIC, 0x19)
 
 
-#define LEDDEV_CNT 1 //设备号个数 
-#define LEDDEV_NAME "out_gpio" //设备名字 /dev/out_gpio
+#define LEDDEV_CNT 2 //设备号个数 
+#define LEDDEV_NAME "rd04gpio" //设备名字 /dev/out_gpio
 
-#define LEDOFF 0 //无物体接近
-#define LEDON 1 //有物体接近
+
+dev_t parent_devid; //设备号
+struct class *class;    //类
 
 /*gpioled设备结构体*/
-struct outgpio_dev
+struct gpio_dev
 {
     dev_t devid;    //设备号
     struct cdev cdev;   //cdev
-    struct class *class; 
     struct device *device;
     struct device_node *nd; //led设备节点
-    int gpio_out;   //led所使用的GPIO标号
-
+    int gpio_en;//led所使用的GPIO标号
+   const char *name;
+   
 };
-struct outgpio_dev outdev;
+struct gpio_dev gpiodev[LEDDEV_CNT];
  
+
 #if 0
-/*
- * @description : 接近结果输出
-* @param - sta : LEDON(0) 打开 LED，LEDOFF(1) 关闭 LED
- * @return : 无
- */
-void led_switch(u8 sta)
+static long rd04_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 {
-    if (sta == LEDON )
-    gpio_set_value(outdev.gpio_out, 1);
-    else if (sta == LEDOFF)
-    gpio_set_value(outdev.gpio_out, 0);
- }
-#endif
+	struct gpio_dev *dev = filp->private_data;
+	int ret;
 
+	switch (cmd)
+	{
+	case SET:
+		gpiod_set_value((struct gpio_desc *)dev->gpio_en, arg);
+		printk(KERN_INFO "rd04 is en.\n");
+		break;
+		
+	case OUT:
+        ret = gpiod_get_value((struct gpio_desc *)dev->gpio_out);
+        if(ret == 0)
+        {
+            printk(KERN_INFO "people in!\n");
+        }
+        else
+        {
+            printk(KERN_INFO "prople out!\n");
+        }
+		return ret;
+	
+	default:
+		return -EINVAL;
+	}
+	
+	return 0;
 
-static int led_gpio_init(struct device_node *nd)
-{
-    int ret = 0;
-    
-    /*1\获取设备树中gpio属性，得到led所使用的led编号*/
-    outdev.gpio_out = of_get_named_gpio(nd,  "led-gpio", 0); //设备编号有platform给
-   if(!gpio_is_valid(outdev.gpio_out))
-    {
-        printk(KERN_ERR "outdev: Failed to get led-gpio\n");
-        return -EINVAL;
-    }
-    /*2\申请使用GPIO*/
-    ret = gpio_request(outdev.gpio_out, "LED0");
-    if (ret) 
-    {
-        printk(KERN_ERR "outdev: Failed to request led-gpio\n");
-        return ret;
-    }
-
-    /* 3、设置 PI0 为输入，检测物体的值输入到GPIO */
-    ret = gpio_direction_input(outdev.gpio_out);
-    if(ret < 0)
-    {
-        printk("can't set gpio!\r\n");
-    }
-    return 0;
 }
+
+#endif
 
 /*
 *@描述          ：打开设备
@@ -99,8 +88,11 @@ static int led_gpio_init(struct device_node *nd)
 *@返回值    ：0成功
 */
 static int rd04_open(struct inode *inode, struct file *filp)
-{   //file结构体中的private_data的成员变量一般在open的时候将该变量指向设备结构体
-    
+{   
+/*在打开多个设备时，需要用container_of函数获取对应的设备描述文件并保持在私有变量中*/
+    struct gpio_dev *dev = container_of(inode->i_cdev, struct gpio_dev, cdev);
+    filp->private_data = dev;
+
     return 0;
 }
 
@@ -118,96 +110,170 @@ static int rd04_open(struct inode *inode, struct file *filp)
     int ret;
     int current_val;
 
-    current_val = gpio_get_value(outdev.gpio_out);
+    current_val = gpio_get_value(gpiodev[1].gpio_en);
     /* 将按键状态信息发送给应用程序 */
     ret = copy_to_user(buf, &current_val, sizeof(int));
-
-#if 0 
-    ledstat = databuf[0];   //获取状态值
-
-    if(ledstat == LEDON) 
-    { 
-        led_switch(LEDON); /* 打开 LED 灯 */
-    } 
-    else if(ledstat == LEDOFF)
-    {
-        led_switch(LEDOFF); /* 关闭 LED 灯 */
-    }
-#endif
 
     return ret;
  }
 
 
  /* 设备操作函数定义 */
-static struct file_operations led_fops = {
+static struct file_operations rd_fops = {
     .owner =    THIS_MODULE,
     .open  =    rd04_open,
     .read =    rd04_read,
-    
+    //.unlocked_ioctl = rd04_ioctl,
 };
 
+/*gpio初始化，需要分别获取各个子节点
+*dev：各个子节点的设备描述
+*return ：成功0失败-1
+*/
+static int gpio_init(struct gpio_dev *dev)
+{
+    int ret;
+    const char *str;
+    dev->gpio_en = of_get_named_gpio(dev->nd,  "gpios", 0); //获取设备树属性gpios名称对应的第一个设备编号
+    printk(KERN_ERR "gpiodev-en: %d\n", dev->gpio_en);
+   if(!gpio_is_valid(dev->gpio_en))
+    {
+        printk(KERN_ERR "gpiodev-en: Failed to get gpios:0\n");
+        return -EINVAL;
+    }
+    /*从设备树的子节点中获取各个label*/
+	ret = of_property_read_string(dev->nd, "label", &str);
+    if (ret < 0)
+	{
+ 
+		printk("of_property_read_string label failed! \r\n");
+ 
+	}
+ 
+	dev->name = str;
+ 
+	printk("of_property_read_string label is %s \r\n", dev->name);
+    /*申请GPIO*/
+    ret = gpio_request(dev->gpio_en, str);	
+    if (ret) 
+    {
+        printk(KERN_ERR "gpiodev-en: Failed to request \n");
+        return ret;
+    }
+ 
+    if(!strcmp(dev->name, "gpio-set" ))
+    {
+
+        /*默认使能端为高*/
+        ret = gpio_direction_output(dev->gpio_en, 1);
+        if(ret < 0)
+        {
+            printk("can't set gpio!\r\n");
+        }
+       
+    }
+   
+    if(!strcmp(dev->name, "gpio-out" ))
+    {
+        ret = gpio_direction_input(dev->gpio_en);
+        if(ret < 0)
+        {
+            printk("can't out gpio!\r\n");
+        }
+    }
+
+  return 0;
+}
 /*
  * @description : flatform 驱动的 probe 函数，当驱动与设备匹配以后此函数
  * 就会执行
 * @param - dev : platform 设备
  * @return : 0，成功;其他负值,失败
  */
- static int led_probe(struct platform_device *pdev)
+ static int rd_probe(struct platform_device *pdev)
 {
-    int ret;
-    
-    printk("led driver and device was matched!\r\n");
-    
-    /* 初始化 LED */
-    ret = led_gpio_init(pdev->dev.of_node);
-    if(ret < 0)
-        return ret;
+    int ret, i;
+    int major_devid;
+	struct device_node *parent_nd;
+    char node_path[32] = { 0 };
 
+
+    parent_nd = pdev->dev.of_node;
+
+ 
+   
+   /*获取子设备节点*/
+   for(i = 0; i < LEDDEV_CNT; i++)
+   {
+        sprintf(node_path, "/gpioled/led%d", i);
+        gpiodev[i].nd = of_find_node_by_path(node_path);
+
+        ret = gpio_init(&gpiodev[i]);
+        if(ret < 0)
+            return ret;
+   }
 
     /*注册字符设备驱动*/
-    /*1\创建设备号*/
+    /*1\创建父设备号*/
+    ret = alloc_chrdev_region(&parent_devid, 0, LEDDEV_CNT, LEDDEV_NAME);//申请设备号
+   if(ret < 0)
+    {
+        pr_err("%s Couldn't alloc_chrdev_region, ret=%d",LEDDEV_NAME, ret);
+        goto free_gpio;
+    }
    
-        ret = alloc_chrdev_region(&outdev.devid, 0, LEDDEV_CNT, LEDDEV_NAME);//申请设备号
+   major_devid = MAJOR(parent_devid);
+   printk("request major_devid is %d \r\n", major_devid);
+
+   for(i = 0; i < LEDDEV_CNT; i++ )
+   {
+        gpiodev[i].devid = parent_devid+i;  //有别于直接使用MKDEV创建，因为parent_devid已经是dev_t类型，包含了主次设备号了
+        /*2、初始化cdev*/
+        gpiodev[i].cdev.owner = THIS_MODULE;
+        cdev_init(&gpiodev[i].cdev, &rd_fops);
+
+        /*3\添加一个cdev*/
+        ret = cdev_add(&gpiodev[i].cdev, gpiodev[i].devid, LEDDEV_CNT);
         if(ret < 0)
-        {
-                pr_err("%s Couldn't alloc_chrdev_region, ret=%d",LEDDEV_NAME, ret);
-                goto free_gpio;
-        }
-
-    /*2、初始化cdev*/
-    outdev.cdev.owner = THIS_MODULE;
-    cdev_init(&outdev.cdev, &led_fops);
-
-    /*3\添加一个cdev*/
-    ret = cdev_add(&outdev.cdev, outdev.devid, LEDDEV_CNT);
-    if(ret < 0)
-        goto del_unregister;
-
+            goto del_unregister;
+   }
     /*4\创建类*/
-    outdev.class = class_create(THIS_MODULE, LEDDEV_NAME);
-    if(IS_ERR(outdev.class))
+    class = class_create(THIS_MODULE, LEDDEV_NAME);
+    if(IS_ERR(class))
     {
         goto del_cdev;
     }
    
-   /*5\创建设备*/
-   outdev.device = device_create(outdev.class, NULL, outdev.devid, NULL, LEDDEV_NAME);
-   if(IS_ERR(outdev.device))
+   for(i = 0; i < LEDDEV_CNT; i++)
    {
-        goto destroy_class;
-   }
-   return 0;
-   
+     
+        /*5\创建次设备*/
+        gpiodev[i].device = device_create(class, NULL, gpiodev[i].devid, NULL, "%s%d",LEDDEV_NAME, i);
+        if(IS_ERR(gpiodev[i].device))
+        {
+            ret = PTR_ERR(gpiodev[i].device);
+                goto destroy_class;
+        }
+  
+    }
 
+
+ 
+    return 0;
+  
     destroy_class:
-        class_destroy(outdev.class);
+        class_destroy(class);
     del_cdev:
-        cdev_del(&outdev.cdev);
+        for(i = 0; i < LEDDEV_CNT; i++)
+        {
+            cdev_del(&gpiodev[i].cdev);
+        }
+        
     del_unregister:
-        unregister_chrdev_region(outdev.devid, LEDDEV_CNT);
+        unregister_chrdev_region(parent_devid, LEDDEV_CNT);
     free_gpio:
-        gpio_free(outdev.gpio_out);
+        gpio_free(gpiodev[0].gpio_en);
+        gpio_free(gpiodev[1].gpio_en);
     return -EIO;
 }
 
@@ -217,32 +283,43 @@ static struct file_operations led_fops = {
  * @param - dev : platform 设备
  * @return : 0，成功;其他负值,失败
  */
-static int led_remove(struct platform_device *dev)
+static int rd_remove(struct platform_device *dev)
 {
-    gpio_set_value(outdev.gpio_out, 1); /* 卸载驱动的时候关闭 LED */
-    gpio_free(outdev.gpio_out); /* 注销 GPIO */
-    cdev_del(&outdev.cdev); /* 删除 cdev */
-    unregister_chrdev_region(outdev.devid, LEDDEV_CNT);
-    device_destroy(outdev.class, outdev.devid); /* 注销设备 */
-    class_destroy(outdev.class); /* 注销类 */
+     int i;
+  
+
+    gpio_set_value(gpiodev[0].gpio_en, 0); /* 卸载驱动的时候关闭 rd04 */
+    gpio_free(gpiodev[0].gpio_en); /* 注销 GPIO */
+    gpio_free(gpiodev[1].gpio_en); /* 注销 GPIO */
+   
+    for(i = 0; i < LEDDEV_CNT; i++)
+   {
+
+        device_destroy(class, gpiodev[i].devid);/* 注销设备 */
+   }
+    class_destroy(class); /* 注销类 */
+    cdev_del(&gpiodev[0].cdev); /* 删除 cdev */
+    cdev_del(&gpiodev[1].cdev); /* 删除 cdev */
+    unregister_chrdev_region(parent_devid, LEDDEV_CNT);
     return 0;
 
 }
 
  /* 匹配列表 */
- static const struct of_device_id led_of_match[] = {
+ static const struct of_device_id rd_of_match[] = {
     {.compatible = "alientek,led"}, //与设备树的设备节点compatible对应
     { /*sentinel*/}
  };
-    MODULE_DEVICE_TABLE(of, led_of_match);//声明led_of_match这个匹配表
+    MODULE_DEVICE_TABLE(of, rd_of_match);//声明led_of_match这个匹配表
  /* platform 驱动结构体 */
-static struct platform_driver led_driver = {
+static struct platform_driver rd_driver = {
     .driver = {
-        .name = "stm32mp1-led", //驱动名字，会在/sys/bus/platform/drivers/目录下存在一个名为“stm32mp1-led”的文件
-        .of_match_table = led_of_match, //设备树匹配表
+        .name = "stm32mp1-rd04", //驱动名字，会在/sys/bus/platform/drivers/目录下存在一个名为“stm32mp1-rd04”的文件
+        .of_match_table = rd_of_match, //设备树匹配表
     },
-    .probe = led_probe,
-    .remove = led_remove,
+    .probe = rd_probe,
+    .remove = rd_remove,
+	
 };
 
 /*
@@ -250,9 +327,12 @@ static struct platform_driver led_driver = {
 *@入参      ：无
 *@返回值    ：无
 */
-static int __init leddriver_init(void)
+static int __init gpiodriver_init(void)
 {
-   return platform_driver_register(&led_driver);
+    int ret;
+    ret = platform_driver_register(&rd_driver);
+     printk(KERN_ERR "gpiodriver init scuess\n");
+   return ret;
 }
 
 
@@ -262,13 +342,13 @@ static int __init leddriver_init(void)
 *@入参      ：无
 *@返回值    ：无
 */
-static void __exit leddriver_exit(void)
+static void __exit gpiodriver_exit(void)
 {
-   platform_driver_unregister(&led_driver);
+   platform_driver_unregister(&rd_driver);
 }
 
-module_init(leddriver_init);
-module_exit(leddriver_exit);
+module_init(gpiodriver_init);
+module_exit(gpiodriver_exit);
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("LZJ");
 MODULE_INFO(intree, "Y");
--- Anti-collision/src/main.c
+++ Anti-collision/src/main.c
@@ -2,12 +2,20 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/ioctl.h>
 #include <fcntl.h>
 #include <stdlib.h>
 #include <string.h>
 #include <pthread.h>
 #include <errno.h>
-/*命令行输入：./main /dev/out_gpio /dev/RD04*/
+
+#define PLATDRV_MAGIC       0x60     //魔术字
+#define SET             _IO (PLATDRV_MAGIC, 0x18)   //没有数据传输的命令
+#define OUT              _IO (PLATDRV_MAGIC, 0x19)
+
+#define OPEN 1
+#define CLOSE 0
+/*命令行输入：./main /dev/out_gpio /dev/gpio_en /dev/RD04*/
 
 /*读取雷达数据*/
 static void *go_rd04(void *s)
@@ -41,58 +49,74 @@ static void *go_rd04(void *s)
 
 int main(int argc, char *argv[])
 {
-   int fd, retvalue;
-    char *filename;
+   int fd_en, fd_out, retvalue;
+    char *filename_out, *filename_en;
     int databuf;
     pthread_t pid;
     int err;
-    if(argc != 3)
+    if(argc != 4)
     {
-        printf("Error Usage:%s /dev/out_gpio!\r\n", argv[0]);
+        printf("Error Usage:%s /dev/gpio_en /dev/gpio_out /dev/RD04*/\r\n", argv[0]);
         return -1;
     }
 
-    filename = argv[1];
+    filename_en = argv[1];
 
-    /* 打开 led 驱动 */
-    fd = open(filename, O_RDWR);
-    if(fd < 0)
+    /* 打开 gpio-en 驱动 */
+    fd_en = open(filename_en, O_RDWR);
+    if(fd_en < 0)
     {
         printf("file %s open failed!\r\n", argv[1]);
+        close(fd_en);
         return -1;
     }
+    ioctl(fd_en, SET, OPEN);
 
-    
-    /* 读/dev/out_gpio  */
-    retvalue = read(fd, &databuf, sizeof(int));
-    if(retvalue < 0)
-    {
-        printf("LED Control Failed!\r\n");
-        close(fd);
-        return -1;
-    }
-    if(databuf == 1)
-    {
-      printf("有人\n");
-    }
-    else
-    {
-      printf("没人\n");
-    }
-
-    err = pthread_create(&pid, NULL, go_rd04, argv[2]);
+    /*创建线程：执行rd04驱动文件*/
+    err = pthread_create(&pid, NULL, go_rd04, argv[3]);
     if(err < 0)
     {
       fprintf(stderr, "pthread_create fail :%s", strerror(errno));
       return -1;
     }
 
-    retvalue = close(fd); /* 关闭文件 */
-    if(retvalue < 0)
+    /*打开结果输出文件*/
+    filename_out = argv[2];
+    fd_out = open(filename_out, O_RDWR);
+     if(fd_out < 0)
     {
-        printf("file %s close failed!\r\n", argv[1]);
+        printf("file %s open failed!\r\n", argv[2]);
+        close(fd_out);
+        close(fd_en);
         return -1;
     }
+    while(1)
+    {
+
+        /* 读/dev/out_gpio  */
+        retvalue = ioctl(fd_out, OUT, 0);
+        if(retvalue < 0)
+        {
+            printf("LED Control Failed!\r\n");
+            close(fd_out);
+            close(fd_en);
+            return -1;
+        }
+        if(retvalue == 1)
+        {
+        printf("有人\n");
+        }
+        else
+        {
+        printf("没人\n");
+        }
+
+        usleep(1000000);
+    }
+
+    close(fd_out); /* 关闭文件 */
+    close(fd_en);
+   
     return 0;
 
 }
